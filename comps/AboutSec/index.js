cd229920cfc7b78827edd70142398646a0bc6e97
yslim20 <ylim27@my.bcit.ca>
2022-01-28 01:25:32 -0800

    finished initial description page

---
+++ comps/AboutSec/index.js
@@ -0,0 +1,136 @@
+import React, { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import styles from '@/styles/Home.module.css';
+import Desc from '@/comps/Desc';
+import DivImg from '@/comps/DivImg';
+
+// ============ CSS ============== //
+const Cont = styled.div`  
+  width: 100%; height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content:space-between;
+  padding: 0 9.02%;
+  position: relative;
+  // overflow: hidden;
+  z-index: -1;  
+  box-sizing: border-box;
+
+  @media only screen and (min-width: 1px) and (max-width: 950px){
+    flex-direction: column;
+    align-items: center;  
+    justify-content center;  
+  }
+`
+
+const ImgCont = styled.div`
+  width: 100%; height: 80%; 
+  background-image: url("./images/img_Background.svg");
+  background-repeat: no-repeat;
+  background-size: 70%;
+  background-position: center center;
+  background-attachment: fixed; 
+  opacity: 0.2;
+  position: absolute;
+  bottom: 0; left: 0;
+  z-index: -999;  
+  box-sizing: border-box;
+
+  @media only screen and (min-width: 1px) and (max-width: 950px){
+    background-size: 100%;
+  }
+`
+
+const DesContL = styled.div`
+  width: 45%; 
+  display: flex;
+  flex-direction: column;  
+  align-items: flex-start;
+  margin-top: ${props => props.marginT}%;
+  box-sizing: border-box;
+  position: relative;
+  top: 30%;
+  
+  @media only screen and (min-width: 1px) and (max-width: 950px){
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    top: 5%;
+    margin-bottom: 48px;
+  }
+`
+
+const DesContR = styled.div`
+  width: 45%; 
+  display: flex;
+  flex-direction: column;  
+  align-items: flex-end;
+  box-sizing: border-box;
+  position: relative;
+  top: 55%;
+  
+  @media only screen and (min-width: 1px) and (max-width: 950px){
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    position: static;
+  }
+`
+
+const DivCont = styled.div`
+  width: 100%;
+  height: 100%;
+  display: block;
+  position: absolute;
+  top: -1px;
+  left: 0px;
+  z-index: -10;
+`
+
+const CirCont = styled.div`
+  width: 4.0625em; height: 4.0625em;
+  background-image: url("./images/img_Circles.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center center;
+  position: absolute;
+  top: -6em;
+  left: 0;
+  z-index: -1;
+
+  @media only screen and (min-width: 461px) and (max-width: 768px){    
+    width: 3em; height: 3em;    
+    transition: all 0.3s;
+  }
+
+  @media only screen and (min-width: 1px) and (max-width:460px){    
+    display: none;
+  }
+`
+
+
+const AboutSec = ({
+
+}) => {
+  return (
+    <Cont>
+      <DivCont>
+        <DivImg />
+      </DivCont>
+
+      <DesContL>
+        <Desc 
+          TxtMarginB = "30"            
+        />
+      </DesContL>
+
+      <DesContR >
+        <CirCont />
+        <Desc flip = {true} />
+      </DesContR>      
+      <ImgCont/>
+    </Cont>
+    );
+}
+
+export default AboutSec;
